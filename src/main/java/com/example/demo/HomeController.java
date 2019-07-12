3782be144eb33536413c935bb1477ba93184dc10
greendb16 <greendb16@gmail.com>
2019-07-12 16:00:04 -0400

    Final

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
     @RequestMapping("/")
     public String homePage(Model model){
         model.addAttribute("myvar", "Say hello to the people.");
-//        model.addAttribute("yourval", "Say goodbye to the people.");
+        model.addAttribute("yourval", "Say goodbye to the people.");
         return "hometemplate";
     }
 }
